f20148913174a4b03354dfe6ac70b8ec7e159842
Edrei <edreifernandes@gmail.com>
2020-11-06 21:07:47 -0300

    Centralizando logo da home

--- frontend/Web/src/components/Home.vue
+++ frontend/Web/src/components/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="row justify-content-center">
-    <div class="jumbotron col-5">
-      <div class="row col-10">
-        <img class="img-fluid" src="@/assets/logo_livres.png">
+    <div class="card col-5">
+      <div class="row justify-content-center">
+        <div class="col-12 my-3">
+          <img class="img-fluid" src="@/assets/logo_livres.png">
+        </div>
       </div>
     </div>
   </div>
